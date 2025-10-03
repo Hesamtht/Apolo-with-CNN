4cb5e89377f9c691d5e54762aefd97e865d7c8ba
Hesam Mozafari <101742111+Hesamtht@users.noreply.github.com>
2025-10-03 21:44:38 +0330

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import *
+from . import views
 
-app_name = 'club'
-
+app_name = 'menu'
 
 urlpatterns = [
-    path('' , user_sub , name = 'subscription'),
+    path('', views.food_list, name='list'),
+    path('<slug:slug>/', views.food_detail, name='detail'),
+    path('add_to_cart/', views.add_to_cart, name='add_to_cart'),
+    path('video_feed/', views.video_feed, name='video_feed'),
 ]
