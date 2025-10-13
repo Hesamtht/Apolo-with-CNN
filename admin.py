cd875f08365238d03952f0cef6fb083621fe6f04
Hesam Mozafari <101742111+Hesamtht@users.noreply.github.com>
2025-10-13 21:05:18 +0330

    Add files via upload

--- admin.py
+++ admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Food
 
 # Register your models here.
+
+
+@admin.register(Food)
+
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ('name' , 'description' , 'rate' , 'price')
+    prepopulated_fields = {'slug' : ('name',)} #auto fill slug when writing food name
--- apps.py
+++ apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ClubConfig(AppConfig):
+class MenuConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'club'
+    name = 'menu'
